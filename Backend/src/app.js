e81010c051f9cadd5074dcb2e787fcb7ff563c3c
cristopherleojs <cristopherleonardo96@gmail.com>
2026-03-27 03:35:06 -0600

    Spotify API por mood completado

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const authRoutes = require("./modules/auth/auth.routes")
 const roomRoutes = require("./modules/rooms/room.routes")
 const skillRoutes = require("./modules/skills/skill.routes")
+const spotifyRoutes = require("./modules/spotify/spotify.routes")
 
 const app = express()
 app.use(cors())
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/rooms", roomRoutes)
 app.use("/api/skills", skillRoutes)
+app.use("/api/spotify", spotifyRoutes)
 
 module.exports = app
