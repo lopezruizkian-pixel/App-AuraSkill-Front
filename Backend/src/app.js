2c242cc5fc28df0ff532fdd50b9ac22591fafc45
kianlopez <233366@ids.upchiapas.edu.mx>
2026-04-21 18:40:53 -0600

    feat: agregar ruta healthcheck en /api para railway

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Healthcheck para Railway
+app.get('/api', (req, res) => {
+  res.send('API funcionando 🚀');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/rooms', roomRoutes);
 app.use('/api/skills', skillRoutes);
